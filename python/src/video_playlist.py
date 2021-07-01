653a77a3b3ec4da905aa5fe055b8860f84a3b81b
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2021-07-01 11:43:15 +0700

    implemented playlist

--- python/src/video_playlist.py
+++ python/src/video_playlist.py
@@ -1,5 +1,22 @@
 """A video playlist class."""
 
+from typing import Sequence
+from .video import Video
 
 class Playlist:
     """A class used to represent a Playlist."""
+
+    def __init__(self, name: str):
+        self._name = name
+        self._videos = {}
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def videos(self) -> Sequence[Video]:
+        return self._videos
+
+    def add(self, video: Video):
+        self._videos[video.video_id] = Video
